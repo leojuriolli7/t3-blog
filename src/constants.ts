9d7b042321285e12a52f2f72237c5d4d77df4594
leojuriolli7 <leojuriolli@gmail.com>
2023-02-20 20:53:34 -0300

    fix: add bypass url for production deployment

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
-export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+const environmentUrl =
+  process.env.NEXT_PUBLIC_BYPASS_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
+
+export const baseUrl = environmentUrl
+  ? `https://${environmentUrl}`
   : `http://localhost:3000`;
 
 export const url = `${baseUrl}/api/trpc`;
