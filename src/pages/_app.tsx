f9f0f4b0cac2a84542bfce86d5559843bc3d8ad7
leojuriolli7 <leojuriolli@gmail.com>
2023-02-24 20:57:44 -0300

    fix: interval

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ import { url } from "@utils/constants";
 
 function App({ Component, pageProps }: AppProps) {
   const { data, isLoading } = trpc.useQuery(["users.me"], {
-    // refetchInterval: 1000,
+    refetchInterval: 1000,
   });
 
   if (isLoading) return <p>Loading user...</p>;
