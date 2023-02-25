930c4c5110959c3a3e6670a827a8d6c47c692558
leojuriolli7 <leojuriolli@gmail.com>
2023-02-25 12:20:27 -0300

    chore: google verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="Th1vj3qhx4-1_Yzg52ScWKjwSkc3QLje1wG5WAuM0rQ"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
