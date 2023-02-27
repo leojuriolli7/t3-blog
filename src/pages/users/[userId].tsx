f750a876ef4855f95303563b875b10ab999e5b0b
leojuriolli7 <leojuriolli@gmail.com>
2023-02-26 22:00:08 -0300

    fix: link underline on empty state

--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -129,7 +129,7 @@ const UserPage: React.FC = () => {
                   It seems you have not created any posts yet.
                 </p>
                 <Link href="/posts/new" passHref>
-                  <a className="text-emerald-500 text-center mt-2">
+                  <a className="text-emerald-500 text-center underline mt-2">
                     Get to it!
                   </a>
                 </Link>
