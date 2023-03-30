c301df1836f3ba259749870d072d4caffbb10ba4
leojuriolli7 <leojuriolli@gmail.com>
2023-03-29 23:12:14 -0300

    refactor: use marked instead of markdownIt, sanitize html with dompurify, fix code block with language issue

--- src/utils/htmlToMarkdown.ts
+++ src/utils/htmlToMarkdown.ts
@@ -2,7 +2,8 @@ import TurndownService from "turndown";
 import { gfm } from "turndown-plugin-gfm";
 
 export default function htmlToMarkdown(html?: string) {
-  const converter = new TurndownService().use(gfm);
+  // `codeBlockStyle` makes language prefixes not be removed on conversion. (eg: ```js)
+  const converter = new TurndownService({ codeBlockStyle: "fenced" }).use(gfm);
 
   return converter.turndown(html || "");
 }
