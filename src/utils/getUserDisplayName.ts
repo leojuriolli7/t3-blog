cee5fa53816c77bc809201eb2031c4d285886c6d
leojuriolli7 <leojuriolli@gmail.com>
2023-04-08 11:12:49 -0300

    fix: getUserDisplayName types

--- src/utils/getUserDisplayName.ts
+++ src/utils/getUserDisplayName.ts
@@ -1,8 +1,9 @@
-import { User } from "@prisma/client";
-import { User as UserType } from "./types";
-import { User as AuthUser } from "next-auth";
+type User = {
+  email?: string | null;
+  name?: string | null;
+};
 
-const getUserDisplayName = (user?: User | UserType | AuthUser | null) => {
+const getUserDisplayName = (user?: User | null) => {
   if (!!user?.name) return user?.name;
 
   if (!!user?.email) return user?.email;
