144ba5d867457d9389df29596b5929409ff0ffea
leojuriolli7 <leojuriolli@gmail.com>
2023-04-23 17:49:38 -0300

    fix: badge props

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,6 +6,7 @@ type Props = React.HTMLAttributes<HTMLSpanElement>;
 export const Badge: React.FC<Props> = (props) => {
   return (
     <span
+      {...props}
       className={clsx(
         "bg-emerald-500 dark:bg-emerald-600 ml-1 text-xs text-white font-bold p-[2px] px-1 shadow-sm select-none",
         props?.className
