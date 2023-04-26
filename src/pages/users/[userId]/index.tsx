74a38c74eb69a5209f9ae0ce5736ab4623f72518
Leonardo Juriolli <leojuriolli@gmail.com>
2023-04-26 13:01:09 -0300

    fix: render follow button only after loading user

--- src/pages/users/[userId]/index.tsx
+++ src/pages/users/[userId]/index.tsx
@@ -238,7 +238,9 @@ const UserPage: NextPage<
             className="h-[240px] w-[240px] rounded-full object-cover"
             alt={user?.name as string}
           />
-          <ShouldRender if={!loggedUserIsProfileOwner && session?.user?.id}>
+          <ShouldRender
+            if={!loggedUserIsProfileOwner && session?.user?.id && !!user}
+          >
             <Button
               loading={loadingUser}
               variant="gradient"
